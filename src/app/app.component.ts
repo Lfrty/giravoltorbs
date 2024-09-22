69bcdc8e7e1ec63650f4f5ef3444c6d454df5368
Lfrty <matiasdelosriosaguilar@gmail.com>
2024-09-22 13:23:29 +0200

    Añadidos componentes panel y tablero. Commit previo a actualización de versión en Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PanelComponent } from "./panel/panel.component";
+import { TableroComponent } from './tablero/tablero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PanelComponent, TableroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
